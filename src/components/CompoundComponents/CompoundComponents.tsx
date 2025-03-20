8d9de993e4814b244eea78ea98ca358af503f70c
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-03-19 20:02:41 -0500

    Patron Compound

--- src/components/CompoundComponents/CompoundComponents.tsx
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -1,6 +1,12 @@
+import Tab from "./Tab";
+import Tabs from "./Tabs";
+
 export const CompoundComponents = () => {
   return (
-    <>
-    </>
+    <Tabs>
+      <Tab label="Girafa">🦒</Tab>
+      <Tab label="Delfín">🐬</Tab>
+      <Tab label="Lagarto">🦎</Tab>
+    </Tabs>
   );
 };
