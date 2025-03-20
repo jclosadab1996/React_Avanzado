a96a48bca6ae7acf426a97c2662470501019a9a0
Juan Camilo Losada Bultaif <92903961+jclosadab1996@users.noreply.github.com>
2025-03-20 18:57:51 -0500

    Control y uncontrolled

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,19 @@
+import { useState } from "react";
+
 export const ControlledInput = () => {
+  const [value, setValue] = useState("");
+
   return (
     <div>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Ingresa el codigo del cupón. EJ: 123456"
+      />
+      <p>
+        Cupón de descuento: <b>{value}</b>
+      </p>
     </div>
   );
 };
